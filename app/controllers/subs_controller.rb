6c1c49c93da0f192505dabf27f6385deb5fb1bc2
Nathan Kane <kaneitalia.rana@gmail.com>
2020-11-18 10:39:20 -0700

    show method

--- app/controllers/subs_controller.rb
+++ app/controllers/subs_controller.rb
@@ -2,4 +2,8 @@ class SubsController < ApplicationController
   def index
     @subs = Sub.all
   end
+
+  def show
+    @subs = Sub.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
   root 'subs#index'
+
+  get "subs/:id", to: "subs#show", as: "subs_yo"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
